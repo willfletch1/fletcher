0e2986aa8ee674c0df4b4421166cb362599c4066
Jon Agirre <jon.agirre@york.ac.uk>
2023-04-21 18:41:55 +0100

    Parameter processing done, test data added

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 import gemmi
+import argparse
+import json
 
-def main ( ) :
+def find_structural_motifs ( filename = "",
+                             residues = [ ],
+                             distance = 0.0 ) :
   # parse arguments here  
-  st = gemmi.read_structure(path)
+  st = gemmi.read_structure ( filename )
   if st.cell.is_crystal():
     st.add_entity_types()
     for chain in st[0]:
@@ -11,6 +15,25 @@ def main ( ) :
       if polymer:
         for residue in polymer:
           for atom in residue:
-            # check neighbours
+            print (atom)
+
+
 if __name__ == '__main__':
-  main()
+  parser = argparse.ArgumentParser ( 
+                    prog='Fletcher',
+                    description='Fletcher will try to find a list of residues within a fixed distance from each other. \nConcept: Federico Sabbaddin & Jon Agirre.',
+                    epilog='Please send bug reports to Jon Agirre: jon.agirre@york.ac.uk' )
+
+  parser.add_argument ( '-f', '--filename', help = "The name of the file to be processed, in PDB or mmCIF format", required = True )                  
+  parser.add_argument ( '-r', '--residues', help = "A list of residues, one-letter code and comma separated, e.g. G,F", default = "F", required = True )                       
+  parser.add_argument ( '-d', '--distance', help = "Specifies how far each of the residues can be from the rest, in Angstroems", default = "0.0", required = True )  
+
+  args = parser.parse_args ( )
+  residue_list = args.residues.split(",")
+  distance = float ( args.distance )
+  
+  print ( "Running Fletcher with the following parameters:\nFilename: ", args.filename, "\nResidue list: ", residue_list, "\nDistance: ", distance )
+  
+  if len ( residue_list ) > 0 and distance > 0.0 :
+    find_structural_motifs ( args.filename, residue_list, args.distance )
+
