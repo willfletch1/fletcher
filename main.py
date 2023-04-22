de369ec0c3a359f1a4df995f5d910f8af0b61e6d
Jon Agirre <jon.agirre@york.ac.uk>
2023-04-22 17:13:14 +0100

    residue parsing complete

--- main.py
+++ main.py
@@ -21,17 +21,18 @@ def find_structural_motifs ( filename = "",
 if __name__ == '__main__':
   parser = argparse.ArgumentParser ( 
                     prog='Fletcher',
-                    description='Fletcher will try to find a list of residues within a fixed distance from each other. \nConcept: Federico Sabbaddin & Jon Agirre.',
+                    description='Fletcher will try to find a list of residues within a fixed distance from the centre of mass. \nConcept: Federico Sabbaddin & Jon Agirre.',
                     epilog='Please send bug reports to Jon Agirre: jon.agirre@york.ac.uk' )
 
   parser.add_argument ( '-f', '--filename', help = "The name of the file to be processed, in PDB or mmCIF format", required = True )                  
-  parser.add_argument ( '-r', '--residues', help = "A list of residues, one-letter code and comma separated, e.g. G,F", default = "F", required = True )                       
+  parser.add_argument ( '-r', '--residues', help = "A list of residues in one-letter code, e.g. GF", default = "GF", required = True )                       
   parser.add_argument ( '-d', '--distance', help = "Specifies how far each of the residues can be from the rest, in Angstroems", default = "0.0", required = True )  
 
   args = parser.parse_args ( )
-  residue_list = args.residues.split(",")
-  distance = float ( args.distance )
   
+  residue_list = gemmi.expand_protein_one_letter_string(args.residues)
+  distance = float ( args.distance )
+
   print ( "Running Fletcher with the following parameters:\nFilename: ", args.filename, "\nResidue list: ", residue_list, "\nDistance: ", distance )
   
   if len ( residue_list ) > 0 and distance > 0.0 :
